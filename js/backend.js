eb6d86959d530635851fcf9151cd708200c08e6b
Ekaterina <katsar14@yahoo.com>
2018-07-05 11:44:11 +0300

    правка

--- js/backend.js
+++ js/backend.js
@@ -9,59 +9,10 @@
   window.backend.downloadData = function (onLoad, onError) {
     loadData(onLoad, onError, DOWNLOAD_URL, 'GET');
   };
-  // window.backend.downloadData = function (onLoad, onError) {
-  //   var xhr = new XMLHttpRequest();
-  //   var url = DOWNLOAD_URL;
-  //   xhr.responseType = 'json';
-
-  //   xhr.addEventListener('load', function () {
-  //     if (xhr.status === SUCCESS_CODE) {
-  //       onLoad(xhr.response);
-  //     } else {
-  //       onError('Произошла ошибка ' + xhr.status + ' ' + xhr.statusText);
-  //     }
-  //   });
-
-  //   xhr.addEventListener('error', function () {
-  //     onError('Произошла ошибка соединения!  Пожалуйста, попробуйте позже');
-  //   });
-
-  //   xhr.addEventListener('timeout', function () {
-  //     onError('Запрос не успел выполниться за ' + xhr.timeout + 'мс');
-  //   });
-
-  //   xhr.open('GET', url);
-
-  //   xhr.send();
-  // };
 
   window.backend.upload = function (onLoad, onError, data) {
     loadData(onLoad, onError, UPLOAD_URL, 'POST', data);
   };
-  // window.backend.upload = function (data, onLoad, onError) {
-  //   var xhr = new XMLHttpRequest();
-  //   var url = UPLOAD_URL;
-  //   xhr.responseType = 'json';
-
-  //   xhr.addEventListener('load', function () {
-  //     if (xhr.status === SUCCESS_CODE) {
-  //       onLoad();
-  //     } else {
-  //       onError();
-  //     }
-  //   });
-
-  //   xhr.addEventListener('error', function () {
-  //     onError();
-  //   });
-
-  //   xhr.addEventListener('timeout', function () {
-  //     onError('Запрос не успел выполниться за ' + xhr.timeout + 'мс');
-  //   });
-
-  //   xhr.open('POST', url);
-  //   xhr.send(data);
-  // };
 
   var loadData = function (onLoad, onError, url, method, data) {
     var xhr = new XMLHttpRequest();
